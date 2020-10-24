445e4af7f428616831ea7b70cd24ccb1a1c042b4
cleberson.chagas <cleberson.chagas@mv.com.br>
2020-10-24 17:53:42 -0300

    documentation, adjust, clean code

--- src/main/java/br/com/hyteck/api/config/CorsConfiguration.java
+++ src/main/java/br/com/hyteck/api/config/CorsConfiguration.java
@@ -1,17 +1,21 @@
 package br.com.hyteck.api.config;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Allow calling endpoints that may have cross domain problems
+ */
 @Configuration
 public class CorsConfiguration {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("https://tecs-tcc.netlify.app", "http://localhost:3000");
             }
         };
